d82eff95249c32edc2b8b81aaacb2d5c125b5fac
bombxbahadur <katwalutsab36@gmail.com>
2025-08-12 02:52:21 +0545

    feat(ui): hide scroll indicator on mobile (show md+ only)

--- src/sections/Hero.js
+++ src/sections/Hero.js
@@ -168,11 +168,11 @@ const Hero = () => {
       </div>
 
       {/* Scroll Indicator */}
-      {!shouldReduceMotion && (
+    {!shouldReduceMotion && (
         <motion.div
           animate={{ y: [0, 10, 0] }}
           transition={{ duration: 2, repeat: Infinity }}
-          className="absolute bottom-8 left-1/2 transform -translate-x-1/2"
+      className="absolute bottom-8 left-1/2 transform -translate-x-1/2 hidden md:block"
         >
           <div className="w-6 h-10 border-2 border-gray-400 dark:border-gray-500 rounded-full flex justify-center" aria-hidden="true">
             <div className="w-1 h-3 bg-gray-400 dark:bg-gray-500 rounded-full mt-2 animate-bounce"></div>
